c8caa706260fa571f1a3bcf0acac6ea7b7bded1e
HydraBruteforce001 <42317700+HydraBruteforce001@users.noreply.github.com>
2021-08-05 19:27:16 +0800

    vendor changes

--- resources/views/livewire/comps/documents-tray.blade.php
+++ resources/views/livewire/comps/documents-tray.blade.php
@@ -1,9 +1,9 @@
 <div class="">
-    <div class="grid grid-flow-col grid-cols-4 gap-4 sm:grid-cols-1 lg:grid-cols-4">
+    <div class="grid grid-flow-col grid-cols-4 gap-4 sm:grid-cols-1 xs:grid-cols-1 lg:grid-cols-4">
         <div class="">
             @livewire('comps.doctray.left-sidebar')
         </div>
-        <div class="col-span-2 pt-2 overflow-hidden">
+        <div class="col-span-2 pt-2 overflow-hidden sm:col-span-1">
             @livewire('comps.doctray.center-content')
         </div>
         <div class="pt-2">
--- vendor/laravel/jetstream/resources/views/components/authentication-card-logo.blade.php
+++ vendor/laravel/jetstream/resources/views/components/authentication-card-logo.blade.php
@@ -1,5 +1,5 @@
 <a href="/">
-    <svg class="h-100 w-100"viewBox="0 0 377 198" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <svg class="block h-56" viewBox="0 0 377 198" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
         <rect width="377" height="198" fill="url(#pattern0)"/>
         <defs>
         <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
