5298955a72c0a09eba96b2dc5afa271bda680a2f
HydraBruteforce001 <takeshi.forbiz@gmail.com>
2021-08-23 14:35:52 +0800

    hi?

--- vendor/laravel/jetstream/resources/views/components/authentication-card.blade.php
+++ vendor/laravel/jetstream/resources/views/components/authentication-card.blade.php
@@ -1,13 +1,9 @@
-<div class="flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0 bg-blue-51">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
     <div>
         {{ $logo }}
     </div>
-    <div>
-       @if (isset($appname))
-       {{ $appname }}
-       @endif
-    </div>
-    <div class="w-full px-6 py-4 mt-6 overflow-hidden shadow-md sm:max-w-md bg-blue-54 sm:rounded-lg">
+
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>
--- vendor/laravel/jetstream/resources/views/components/checkbox.blade.php
+++ vendor/laravel/jetstream/resources/views/components/checkbox.blade.php
@@ -1,1 +1,1 @@
-<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-300 text-indigo-600 shadow-sm focus:border-blue-51 focus:ring focus:ring-blue-54 focus:ring-opacity-50']) !!}>
+<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
