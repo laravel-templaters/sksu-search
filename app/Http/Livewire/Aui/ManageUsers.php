b64b5459d136af916654ad0563ae95da15af00bb
johnreynaceda <johnreynaceda@sksu.edu.ph>
2021-08-30 15:03:05 +0800

    problem on manageusers

--- app/Http/Livewire/Aui/ManageUsers.php
+++ app/Http/Livewire/Aui/ManageUsers.php
@@ -9,12 +9,13 @@ use App\Models\User;
 class ManageUsers extends Component
 {
     public $selectedDept="";
+    
     public function render()
     { 
         
-        $departments = Departments::get();
+        $departments = Departments::get();  
         $users = User::where('department_id', $this->selectedDept == "" ? 1 : $this->selectedDept)->get();
         return view('livewire.aui.manage-users')
-        ->layout('layouts.admin')->with('departments', $departments)->with('users', $users);
+        ->layout('layouts.app')->with('departments', $departments)->with('users', $users);
     }
 }
